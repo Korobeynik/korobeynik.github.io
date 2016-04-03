60db928280a1ae14ae11e7ddfe7366df7cfe35bd
Korobeynik Yura <korobeynik77@gmail.com>
2016-04-03 17:26:58 +0300

    Add virtualenv and setup pelican.

--- Travelo/js/common.js
+++ Travelo/js/common.js
@@ -1,37 +1,33 @@
 $(function() {
 
-	//Owl carousel
-  $(".carousel").owlCarousel({
-  	loop:true,
-  	responsiveClass:true,
-    responsive:{
-        0:{
-            items:1,
-        },
-        520:{
-            items:1,
-        },
-         560:{
-            items:2,
-        },
-        768:{
-            items:2,
-        },
-        992:{
-            items:3,
-        },
-         1200:{
-            items:4,
-        }
-    }
-  });
+	$(".carousel-eq").owlCarousel({
+		loop:true,
+		responsive:{
+				0:{
+						items:1,
+				},
+				520:{
+						items:1,
+				},
+				560:{
+						items:2,
+				},
+				768:{
+						items:2,
+				},
+				992:{
+						items:3,
+				},
+				1200:{
+						items:4,
+				}
+		}
+	});
 
+	$(".s-direct .item-vertical p").equalHeights();
+	$(".carousel-text").equalHeights();
 
-	//Выравнивание элемента
-	$(".s-direct .item-standart p").equalHeights();
-	$(".s-provision .carousel-item p").equalHeights();
-
-	$(".portfolio-item").each(function(e){
+	$(".portfolio-item").each(function(e) {
 
 		var th = $(this);
 
@@ -44,52 +40,10 @@ $(function() {
 	$(".portfolio-item").magnificPopup({
 		mainClass: 'my-mfp-zoom-in',
 		removalDelay: 300,
-		type: 'inline'
+		type: 'inline',
 	});
 
-	/*
- * Replace all SVG images with inline SVG
- */
-$('img.img-svg').each(function(){
-    var $img = $(this);
-    var imgID = $img.attr('id');
-    var imgClass = $img.attr('class');
-    var imgURL = $img.attr('src');
-
-    $.get(imgURL, function(data) {
-        // Get the SVG tag, ignore the rest
-        var $svg = $(data).find('svg');
-
-        // Add replaced image's ID to the new SVG
-        if(typeof imgID !== 'undefined') {
-            $svg = $svg.attr('id', imgID);
-        }
-        // Add replaced image's classes to the new SVG
-        if(typeof imgClass !== 'undefined') {
-            $svg = $svg.attr('class', imgClass+' replaced-svg');
-        }
-
-        // Remove any invalid XML tags as per http://validator.w3.org
-        $svg = $svg.removeAttr('xmlns:a');
-
-        // Check if the viewport is set, if the viewport is not set the SVG wont't scale.
-        if(!$svg.attr('viewBox') && $svg.attr('height') && $svg.attr('width')) {
-            $svg.attr('viewBox', '0 0 ' + $svg.attr('height') + ' ' + $svg.attr('width'))
-        }
-
-        // Replace image with new SVG
-        $img.replaceWith($svg);
-
-    }, 'xml');
-
-	});
-
-
-
-
-
-	//Galerry
-	$(".mp-gallery").each(function() {
+	$(".mfp-gallery").each(function() {
 		$(this).magnificPopup({
 			delegate: 'a',
 			mainClass: 'mfp-zoom-in',
@@ -101,7 +55,7 @@ $('img.img-svg').each(function(){
 			removalDelay: 300,
 			callbacks: {
 				beforeChange: function() {
-					this.items[0].src = this.items[0].src + '?=' + Math.random();
+					this.items[0].src = this.items[0].src + '?=' + Math.random(); 
 				},
 				open: function() {
 					$.magnificPopup.instance.next = function() {
@@ -115,7 +69,7 @@ $('img.img-svg').each(function(){
 						setTimeout(function() { $.magnificPopup.proto.prev.call(self); }, 120);
 					}
 				},
-				imageLoadComplete: function() {
+				imageLoadComplete: function() { 
 					var self = this;
 					setTimeout(function() { self.wrap.addClass('mfp-image-loaded'); }, 16);
 				}
@@ -123,55 +77,43 @@ $('img.img-svg').each(function(){
 		});
 	});
 
-
-$(".mouse-icon").click(function(){
-	$("html, body").animate({
-		scrollTop : $(".s-advantages").offset().top
-	}, 700)
-});
-
-//jQuery Number Animation with Blur
-	$(".s-advantages").waypoint(function() {
-		$({blurRadius: 5}).animate({blurRadius: 0}, {
-	duration: 1200,
-	easing: 'swing',
-	step: function() {
-		$(".adv-item span").css({
-			"-webkit-filter": "blur("+this.blurRadius+"px)",
-			"filter": "blur("+this.blurRadius+"px)"
-		});
-	}
-});
-	var comma_separator_number_step = $.animateNumber.numberStepFactories.separator(' ');
-	$(".adv-item span").each(function() {
-		var tcount = $(this).data("count");
-		$(this).animateNumber({ number: tcount,
-			easing: 'easeInQuad',
-			"font-size": "1.875em",
-			numberStep: comma_separator_number_step},
-			1200);
+	$(".mouse-icon").click(function() {
+		$("html, body").animate({
+			scrollTop : $(".s-adv").offset().top
+		}, 800);
 	});
 
+	$(".s-adv").waypoint(function() {
+		
+		$({blurRadius: 5}).animate({blurRadius: 0}, {
+			duration: 1200,
+			easing: 'swing',
+			step: function() {
+				$(".s-adv-item h3 span").css({
+					"-webkit-filter": "blur("+this.blurRadius+"px)",
+					"filter": "blur("+this.blurRadius+"px)"
+				});
+			}
+		});
+		var comma_separator_number_step = $.animateNumber.numberStepFactories.separator(' ');
+		$(".s-adv-item h3 span").each(function() {
+			var tcount = $(this).data("count");
+			$(this).animateNumber({ number: tcount,
+				easing: 'easeInQuad',
+				"font-size": "1.8125em",
+				numberStep: comma_separator_number_step},
+				1200);
+		});
 		this.destroy();
+
 	}, {
 		offset: '70%'
 	});
 
-
-
-
-
-$(".mouse-icon").click(function(){
-	$("html, body").animate({
-		scrollTop : $(".s-advantages").offset().top
-	}, 700)
-});
-
-// Mobile menu
-		$(".toggle-mnu").click(function() {
-	  $(this).toggleClass("on");
-	  $(".main-menu").slideToggle();
-	  return false;
+	$(".toggle-mnu").click(function() {
+		$(this).toggleClass("on");
+		$(".main-mnu").slideToggle();
+		return false;
 	});
 
 	//SVG Fallback
@@ -210,5 +152,40 @@ $(".mouse-icon").click(function(){
 	};
 
 	$("img, a").on("dragstart", function(event) { event.preventDefault(); });
+
+	//Replace all SVG images with inline SVG
+	$('img.img-svg').each(function(){
+		var $img = $(this);
+		var imgID = $img.attr('id');
+		var imgClass = $img.attr('class');
+		var imgURL = $img.attr('src');
+
+		$.get(imgURL, function(data) {
+				// Get the SVG tag, ignore the rest
+				var $svg = $(data).find('svg');
+
+				// Add replaced image's ID to the new SVG
+				if(typeof imgID !== 'undefined') {
+					$svg = $svg.attr('id', imgID);
+				}
+				// Add replaced image's classes to the new SVG
+				if(typeof imgClass !== 'undefined') {
+					$svg = $svg.attr('class', imgClass+' replaced-svg');
+				}
+
+				// Remove any invalid XML tags as per http://validator.w3.org
+				$svg = $svg.removeAttr('xmlns:a');
+
+				// Check if the viewport is set, if the viewport is not set the SVG wont't scale.
+				if(!$svg.attr('viewBox') && $svg.attr('height') && $svg.attr('width')) {
+					$svg.attr('viewBox', '0 0 ' + $svg.attr('height') + ' ' + $svg.attr('width'))
+				}
+
+				// Replace image with new SVG
+				$img.replaceWith($svg);
+
+			}, 'xml');
+
+	});
 
 });
