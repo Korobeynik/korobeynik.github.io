9f9df2e07b0303757a3e7c8b5b70fbcc6543070b
Korobeynik Yura <korobeynik77@gmail.com>
2016-04-03 17:58:34 +0300

    Add virtualenv and setup pelican.

--- Travelo/js/common.js
+++ Travelo/js/common.js
@@ -1,72 +1,37 @@
 $(function() {
 
-	$("form select").selectize();
+	//Owl carousel
+  $(".carousel").owlCarousel({
+  	loop:true,
+  	responsiveClass:true,
+    responsive:{
+        0:{
+            items:1,
+        },
+        520:{
+            items:1,
+        },
+         560:{
+            items:2,
+        },
+        768:{
+            items:2,
+        },
+        992:{
+            items:3,
+        },
+         1200:{
+            items:4,
+        }
+    }
+  });
 
-	$(".carousel-brands").owlCarousel({
-		loop:true,
-		margin: 30,
-		nav: true,
-		navText: ["<i class='fa fa-angle-left'></i>","<i class='fa fa-angle-right'></i>"],
-		responsive:{
-				0:{
-						items:1,
-				},
-				520:{
-						items:1,
-				},
-				560:{
-						items:2,
-				},
-				768:{
-						items:2,
-				},
-				992:{
-						items:3,
-				},
-				1200:{
-						items:4,
-				}
-		}
-	});
 
-	$(".carousel-eq").owlCarousel({
-		loop:true,
-		responsive:{
-				0:{
-						items:1,
-				},
-				520:{
-						items:1,
-				},
-				560:{
-						items:2,
-				},
-				768:{
-						items:2,
-				},
-				992:{
-						items:3,
-				},
-				1200:{
-						items:4,
-				}
-		}
-	});
+	//Выравнивание элемента
+	$(".s-direct .item-standart p").equalHeights();
+	$(".s-provision .carousel-item p").equalHeights();
 
-	function heightses() {
-		$(".s-direct .item-vertical p").height('auto').equalHeights();
-		$(".carousel-text").height('auto').equalHeights();
-		$(".testimonials-head").height('auto').equalHeights();
-		$(".testimonials-desc").height('auto').equalHeights();
-	}
-
-	$(window).resize(function() {
-		heightses();
-	});
-
-	heightses();
-
-	$(".portfolio-item").each(function(e) {
+	$(".portfolio-item").each(function(e){
 
 		var th = $(this);
 
@@ -79,10 +44,52 @@ $(function() {
 	$(".portfolio-item").magnificPopup({
 		mainClass: 'my-mfp-zoom-in',
 		removalDelay: 300,
-		type: 'inline',
+		type: 'inline'
 	});
 
-	$(".mfp-gallery").each(function() {
+	/*
+ * Replace all SVG images with inline SVG
+ */
+$('img.img-svg').each(function(){
+    var $img = $(this);
+    var imgID = $img.attr('id');
+    var imgClass = $img.attr('class');
+    var imgURL = $img.attr('src');
+
+    $.get(imgURL, function(data) {
+        // Get the SVG tag, ignore the rest
+        var $svg = $(data).find('svg');
+
+        // Add replaced image's ID to the new SVG
+        if(typeof imgID !== 'undefined') {
+            $svg = $svg.attr('id', imgID);
+        }
+        // Add replaced image's classes to the new SVG
+        if(typeof imgClass !== 'undefined') {
+            $svg = $svg.attr('class', imgClass+' replaced-svg');
+        }
+
+        // Remove any invalid XML tags as per http://validator.w3.org
+        $svg = $svg.removeAttr('xmlns:a');
+
+        // Check if the viewport is set, if the viewport is not set the SVG wont't scale.
+        if(!$svg.attr('viewBox') && $svg.attr('height') && $svg.attr('width')) {
+            $svg.attr('viewBox', '0 0 ' + $svg.attr('height') + ' ' + $svg.attr('width'))
+        }
+
+        // Replace image with new SVG
+        $img.replaceWith($svg);
+
+    }, 'xml');
+
+	});
+
+
+
+
+
+	//Galerry
+	$(".mp-gallery").each(function() {
 		$(this).magnificPopup({
 			delegate: 'a',
 			mainClass: 'mfp-zoom-in',
@@ -94,7 +101,7 @@ $(function() {
 			removalDelay: 300,
 			callbacks: {
 				beforeChange: function() {
-					this.items[0].src = this.items[0].src + '?=' + Math.random(); 
+					this.items[0].src = this.items[0].src + '?=' + Math.random();
 				},
 				open: function() {
 					$.magnificPopup.instance.next = function() {
@@ -108,7 +115,7 @@ $(function() {
 						setTimeout(function() { $.magnificPopup.proto.prev.call(self); }, 120);
 					}
 				},
-				imageLoadComplete: function() { 
+				imageLoadComplete: function() {
 					var self = this;
 					setTimeout(function() { self.wrap.addClass('mfp-image-loaded'); }, 16);
 				}
@@ -116,43 +123,55 @@ $(function() {
 		});
 	});
 
-	$(".mouse-icon").click(function() {
-		$("html, body").animate({
-			scrollTop : $(".s-adv").offset().top
-		}, 800);
+
+$(".mouse-icon").click(function(){
+	$("html, body").animate({
+		scrollTop : $(".s-advantages").offset().top
+	}, 700)
+});
+
+//jQuery Number Animation with Blur
+	$(".s-advantages").waypoint(function() {
+		$({blurRadius: 5}).animate({blurRadius: 0}, {
+	duration: 1200,
+	easing: 'swing',
+	step: function() {
+		$(".adv-item span").css({
+			"-webkit-filter": "blur("+this.blurRadius+"px)",
+			"filter": "blur("+this.blurRadius+"px)"
+		});
+	}
+});
+	var comma_separator_number_step = $.animateNumber.numberStepFactories.separator(' ');
+	$(".adv-item span").each(function() {
+		var tcount = $(this).data("count");
+		$(this).animateNumber({ number: tcount,
+			easing: 'easeInQuad',
+			"font-size": "1.875em",
+			numberStep: comma_separator_number_step},
+			1200);
 	});
 
-	$(".s-adv").waypoint(function() {
-		
-		$({blurRadius: 5}).animate({blurRadius: 0}, {
-			duration: 1200,
-			easing: 'swing',
-			step: function() {
-				$(".s-adv-item h3 span").css({
-					"-webkit-filter": "blur("+this.blurRadius+"px)",
-					"filter": "blur("+this.blurRadius+"px)"
-				});
-			}
-		});
-		var comma_separator_number_step = $.animateNumber.numberStepFactories.separator(' ');
-		$(".s-adv-item h3 span").each(function() {
-			var tcount = $(this).data("count");
-			$(this).animateNumber({ number: tcount,
-				easing: 'easeInQuad',
-				"font-size": "1.8125em",
-				numberStep: comma_separator_number_step},
-				1200);
-		});
 		this.destroy();
-
 	}, {
 		offset: '70%'
 	});
 
-	$(".toggle-mnu").click(function() {
-		$(this).toggleClass("on");
-		$(".main-mnu").slideToggle();
-		return false;
+
+
+
+
+$(".mouse-icon").click(function(){
+	$("html, body").animate({
+		scrollTop : $(".s-advantages").offset().top
+	}, 700)
+});
+
+// Mobile menu
+		$(".toggle-mnu").click(function() {
+	  $(this).toggleClass("on");
+	  $(".main-menu").slideToggle();
+	  return false;
 	});
 
 	//SVG Fallback
@@ -180,41 +199,16 @@ $(function() {
 		return false;
 	});
 
+	//Chrome Smooth Scroll
+	try {
+		$.browserSelector();
+		if($("html").hasClass("chrome")) {
+			$.smoothScroll();
+		}
+	} catch(err) {
+
+	};
+
 	$("img, a").on("dragstart", function(event) { event.preventDefault(); });
-
-	//Replace all SVG images with inline SVG
-	$('img.img-svg').each(function(){
-		var $img = $(this);
-		var imgID = $img.attr('id');
-		var imgClass = $img.attr('class');
-		var imgURL = $img.attr('src');
-
-		$.get(imgURL, function(data) {
-				// Get the SVG tag, ignore the rest
-				var $svg = $(data).find('svg');
-
-				// Add replaced image's ID to the new SVG
-				if(typeof imgID !== 'undefined') {
-					$svg = $svg.attr('id', imgID);
-				}
-				// Add replaced image's classes to the new SVG
-				if(typeof imgClass !== 'undefined') {
-					$svg = $svg.attr('class', imgClass+' replaced-svg');
-				}
-
-				// Remove any invalid XML tags as per http://validator.w3.org
-				$svg = $svg.removeAttr('xmlns:a');
-
-				// Check if the viewport is set, if the viewport is not set the SVG wont't scale.
-				if(!$svg.attr('viewBox') && $svg.attr('height') && $svg.attr('width')) {
-					$svg.attr('viewBox', '0 0 ' + $svg.attr('height') + ' ' + $svg.attr('width'))
-				}
-
-				// Replace image with new SVG
-				$img.replaceWith($svg);
-
-			}, 'xml');
-
-	});
 
 });
