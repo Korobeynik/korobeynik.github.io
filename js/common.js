45efda76c6967244a118fa63482a352c77fd9531
Korobeynik <korobeynik77@gmail.com>
2018-11-02 16:18:36 +0200

    Spello

--- js/common.js
+++ js/common.js
@@ -1,327 +1,246 @@
+onResize = function() {
+    if($(window).width() < 530) {
+        var owl = $('.principles-slider');
+			    owl.owlCarousel({
+			        items:1,
+			        loop:true,
+			        margin: 30,
+			        smartSpeed: 500,
+			        nav: true
+			    });
+    }
+}
+
+$(document).ready(onResize);
+$(window).resize(onResize);
+
+
 $(function() {
 
-	$('.popup').magnificPopup();
-	// $('.close-popup').click( function() {
-	// 	magnificPopup.close(); 
-	// });
+	$(".content-tittle").animated("flipInY", "fadeOutDown");
 
-	$('.close-popup').on('click',function(){                                                      
-    $.magnificPopup.proto.close.call(this);
-	}); 
+	//$(".scroll-to, .icon").mPageScroll2id();
 
-	$('.header__top a[href^="#"]').click(function() {
+	$('.scroll-to').bind('click', function(event) {
+      var $anchor = $(this);
+      $('.st-pusher').stop().animate({
+          scrollTop: ($($anchor.attr('href')).offset().top - 0)
+      }, 1250, 'easeInOutCubic');
+      event.preventDefault();
+  });
 
-		$('html,body').animate({ scrollTop: $(this.hash).offset().top}, 500);
+// $('.scroll-to, .icon').on('click', function (el) {
+//   el.preventDefault();
 
-		return false;
+//   $('html, body').animate({
+//     scrollTop: $($(this).attr('href')).offset().top
+//   }, 500, 'linear');
+// });
+//   $('html, body').animate({
+//     scrollTop: ($('#element').offset().top)
+// },500);
 
-		e.preventDefault();
+  $('.st-menu .icon').click(function(e) {
+  	// e.preventDefault();
+  	// let $anchor = $(this);
+  	// $('.menu-button').trigger('click');
 
-	});
+		// setTimeout(function() {
+		// 	$('.st-pusher').stop().animate({
+  //         scrollTop: ($($anchor.attr('href')).offset().top)
+  //     }, 1500, 'easeInOutCubic');
+		// },1000);		
 
-	//Slider
-	$('.slider').slick({
-	centerMode: false,
-  dots: false,
-  infinite: true,
-  speed: 300,
-  slidesToShow: 4,
-  slidesToScroll: 1,
-  responsive: [
-    {
-      breakpoint: 1024,
-      settings: {
-        slidesToShow: 3,
-        slidesToScroll: 3,
-        infinite: true,
-        dots: false
-      }
-    },
-    {
-      breakpoint: 600,
-      settings: {
-        slidesToShow: 2,
-        slidesToScroll: 2
-      }
-    },
-    {
-      breakpoint: 480,
-      settings: {
-        slidesToShow: 1,
-        slidesToScroll: 1
-      }
-    }
-    // You can unslick at a given breakpoint now by adding:
-    // settings: "unslick"
-    // instead of a settings object
-  ]
+		// setTimeout(function() {
+		// 	$('.st-pusher').animate({
+		// 		scrollTop: $($($anchor).attr('href')).offset().top
+		// 	}, 1000, 'linear');
+		// },1000);		
+
+      // var $anchor = $(this);
+      // $('.st-content-inner').stop().animate({
+      //     scrollTop: ($($anchor.attr('href')).offset().top - 0)
+      // }, 1250, 'easeInOutCubic');
+      // event.preventDefault();
+  });
+
+$(window).scroll(function () {
+
+	console.log($('.st-pusher').scrollTop());
+
+  // if ($(this).scrollTop() > 100) {
+  //   // бла-бла-бла
+  // } else {
+  //   // бла-бла-бла
+  // }
 });
-				
 
-  check();
-	function check() {
+	$('.menu-button').click(function() {
+		//console.log($(this).scrollTop(1000));
+		//$('.st-container').addClass('fixed').scrollTop(1000);
+		$(this).toggleClass('open');
+		$('.st-menu').toggleClass('active');
+		$(this).children('div').toggleClass('active');
+		$("#st-container").toggleClass('st-menu-open');
+		$(".st-container").addClass('open');
+		$(".st-pusher").addClass('open');
 
-		$(window).load( function(){
-			if ($('.flter__item input').is(":checked")) {
-					$(this).next('label').addClass('active');
-				}
-		});
-
-		$('.flter__item input').click( function(){
-			if ($(this).is(":checked")) {
-				$('.flter__item label').removeClass('active');
-				$(this).next('label').addClass('active');
-			} else {
-				$('label').removeClass('active');
-			}
-		});
-
-	};
-
-	//E-mail Ajax Send
-	//Documentation & Example: https://github.com/agragregra/uniMail
-	$("form").submit(function() { //Change
-		var th = $(this);
-		$.ajax({
-			type: "POST",
-			url: "mail.php", //Change
-			data: th.serialize()
-		}).done(function() {
-			alert("Thank you!");
+		if(!$(this).hasClass('open')) {
+			$(this).removeClass('open');
 			setTimeout(function() {
-				// Done Functions
-				th.trigger("reset");
-			}, 1000);
-		});
-		return false;
-	});
-
-	//Chrome Smooth Scroll
-	try {
-		$.browserSelector();
-		if($("html").hasClass("chrome")) {
-			$.smoothScroll();
+				$('.st-container').addClass('fixed');
+				$(".st-pusher").removeClass('open');
+				// $('html, body, .st-container').removeClass('active');
+			},1000);
+		} else {
+			$(this).addClass('open');
 		}
-	} catch(err) {
+	});
 
-	};
+	$('#play').click(function() {
+		$(this).css('display', 'none');
+		$('video').attr('controls',true);
+		$('video').trigger('play');
+	});
 
-	$("img, a").on("dragstart", function(event) { event.preventDefault(); });
-	
-});
-
-$(window).load(function() {
-
-	$(".loader_inner").fadeOut();
-	$(".loader").delay(400).fadeOut("slow");
-	check();
-	function check() {
-		if ($('.flter__item input').is(":checked")) {
-				$(this).next('label').addClass('active');
-			}
-	};
 
 });
 
 
-/************************************* Shoping Cart ***************************************************/
 
-$(document).ready(function() {
+/*!
+ * classie - class helper functions
+ * from bonzo https://github.com/ded/bonzo
+ * 
+ * classie.has( elem, 'my-class' ) -> true/false
+ * classie.add( elem, 'my-new-class' )
+ * classie.remove( elem, 'my-unwanted-class' )
+ * classie.toggle( elem, 'my-class' )
+ */
 
-	// $('.popup').magnificPopup();
+/*jshint browser: true, strict: true, undef: true */
+/*global define: false */
 
-	$("input").click(function () {
-  
-	});
+( function( window ) {
 
+'use strict';
 
-	// $("#checkout").click(function(event) {
-	//    if (totalTax() >= 100){
-	//     prompt("Would you like to pay with Cash or Credit?");
-	//   }else {
-	//     alert("Cash Only.");
-	//   }
-	// });
+// class helper functions from bonzo https://github.com/ded/bonzo
 
-	$("#clear-cart").click(function(event){
-	  clearCart();
-	  displayCart();
-	});
+function classReg( className ) {
+  return new RegExp("(^|\\s+)" + className + "(\\s+|$)");
+}
 
-	$(".add-to-cart").click(function(event) {
-	  event.preventDefault();
-	  var name = $(this).attr("data-name");
-	  var price = Number($(this).attr("data-price"));
-	  addItemToCart(name, price, 1);
-	  displayCart();
-	});
+// classList support for class management
+// altho to be fair, the api sucks because it won't accept multiple classes at once
+var hasClass, addClass, removeClass;
 
-	$("#show-cart").on("click", ".delete-item", function(event) {
-	  var name = $(this).attr("data-name");
-	  removeItemFromCartAll(name);
-	  displayCart();
-	});
+if ( 'classList' in document.documentElement ) {
+  hasClass = function( elem, c ) {
+    return elem.classList.contains( c );
+  };
+  addClass = function( elem, c ) {
+    elem.classList.add( c );
+  };
+  removeClass = function( elem, c ) {
+    elem.classList.remove( c );
+  };
+}
+else {
+  hasClass = function( elem, c ) {
+    return classReg( c ).test( elem.className );
+  };
+  addClass = function( elem, c ) {
+    if ( !hasClass( elem, c ) ) {
+      elem.className = elem.className + ' ' + c;
+    }
+  };
+  removeClass = function( elem, c ) {
+    elem.className = elem.className.replace( classReg( c ), ' ' );
+  };
+}
 
-	$("#show-cart").on("click", ".subtract-item", function(event) {
-	  var name = $(this).attr("data-name");
-	  removeItemFromCart(name);
-	  displayCart();
-	});
+function toggleClass( elem, c ) {
+  var fn = hasClass( elem, c ) ? removeClass : addClass;
+  fn( elem, c );
+}
 
-	$("#show-cart").on("click", ".plus-item", function(event) {
-	  var name = $(this).attr("data-name");
-	  addItemToCart(name, 0, 1);
-	  displayCart();
-	});
+var classie = {
+  // full names
+  hasClass: hasClass,
+  addClass: addClass,
+  removeClass: removeClass,
+  toggleClass: toggleClass,
+  // short names
+  has: hasClass,
+  add: addClass,
+  remove: removeClass,
+  toggle: toggleClass
+};
 
-	//***********************
-	//shopping cart functions
-	/*function cashOrCredit() {
-	  if (totalTax() >= 100){
-	    prompt("Would you like to pay with Cash or Credit?");
-	  }else {
-	    alert("Cash Only.");
-	  }
-	};*/
+// transport
+if ( typeof define === 'function' && define.amd ) {
+  // AMD
+  define( classie );
+} else {
+  // browser global
+  window.classie = classie;
+}
 
-	var cart = [];
-	var Item = function(name, price, count) {
-	  this.name = name
-	  this.price = price
-	  this.count = count
-	};
-
-	function displayCart() {
-	  console.log("***displayCart***");
-	  var cartArray = listCart();
-	  console.log("countCart "+cartArray.length);
-	  var output = "";
-	  for (var i in cartArray) {
-	    output += "<li>" 
-	    	+"<div class='name-product'>"
-	      + cartArray[i].name + " " 
-	      +"</div>"
-	      +"<div class='cart-content'>"
-	      +"<div class='count-wrap'>"
-	      +" <button class='subtract-item' data-name='"
-	      +cartArray[i].name+"'>-</button>"
-	      +"<span>"
-	      + cartArray[i].count 
-	      +"</span>"
-	      +" <button class='plus-item' data-name='"
-	      +cartArray[i].name+"'>+</button>"
-	      +"</div>"
-	      +"<div class='price'>"
-	      + cartArray[i].price +"P"
-	      +"</div>"
-	      +"<div class='total-price'>" 
-	      + cartArray[i].total +"P"
-	      +"</div>" 
-	      +" <button class='delete-item' data-name='"
-	      +cartArray[i].name+"'></button>"
-	      +"</div>"
-	      + "</li>"
-	  }
-	  $("#show-cart").html(output);
-	  $("#count-cart").html(countCart());
-	  $("#sub-total").html(totalCart());
-	  $("#total").html(totalTax());
-	};
-
-	function addItemToCart(name, price, count) {
-	  var item = new Item(name, price, count);
-	  for (var i in cart) {
-	    if (cart[i].name === name) {
-	      cart[i].count += count;
-	      return;
-	    }
-	  }
-	  cart.push(item);
-	  saveCart();
-	};
-
-	function removeItemFromCart(name) { 
-	  for (var i in cart) {
-	    if (cart[i].name === name) {
-	      cart[i].count--; 
-	      if (cart[i].count === 0) {
-	        cart.splice(i, 1);
-	      }
-	      break;
-	    }
-	  }
-	  saveCart();
-	};
-
-	function removeItemFromCartAll(name) {
-	  for (var i in cart) {
-	    if (cart[i].name === name) {
-	      cart.splice(i, 1)
-	      break;
-	    }
-	  }
-	  saveCart();
-	};
-
-	function clearCart() {
-	  cart = [];
-	  saveCart();
-	};
-
-	function countCart() { 
-	  var totalCount = 0;
-	  for (var i in cart) {
-	    totalCount += cart[i].count;
-	    if (totalCount == 2) {
-	    	$('#count-text').html('Подарочных набора');
-	    } 
-	    else if (totalCount >= 5) {
-	    	$('#count-text').html('Подарочных наборов');
-	    }
-	    else {
-	    	$('#count-text').html('Подарочный набор');
-	    }
-	  }
-	  return totalCount;
-	};
-
-	function totalCart() { 
-	  var totalCost = 0;
-	  for (var i in cart) {
-	    totalCost += cart[i].price * cart[i].count;
-	  }
-	  return totalCost.toFixed(2);
-	};
-
-	function totalTax() { 
-	  var totalTax = 0;
-	  for (var i in cart) {
-	    totalTax += (cart[i].price * cart[i].count)*1.075;
-	  }
-	  return totalTax.toFixed(2);
-	};
-
-	function listCart() { 
-	  var cartCopy = [];
-	  for (var i in cart) {
-	    var item = cart[i];
-	    var itemCopy = {}
-	    for (var p in item) {
-	      itemCopy[p] = item[p];
-	    }
-	    itemCopy.total = (item.price * item.count).toFixed(2);
-	    cartCopy.push(itemCopy);
-	  }
-	  return cartCopy;
-	};
-
-	function saveCart() {
-	  localStorage.setItem("shoppingCart", JSON.stringify(cart));
-	};
-
-	function loadCart() {
-	  cart = JSON.parse(localStorage.getItem("shoppingCart"));
-	};
-});
+})( window );
 
 
+/* Sidebar Effects */
+
+//  let SidebarMenuEffects = (function() {
+
+//  	function hasParentClass( e, classname ) {
+// 		if(e === document) return false;
+// 		if( classie.has( e, classname ) ) {
+// 			return true;
+// 		}
+// 		return e.parentNode && hasParentClass( e.parentNode, classname );
+// 	}
+
+// 	function mobilecheck() {
+// 		var check = false;
+// 		(function(a){if(/(android|ipad|playbook|silk|bb\d+|meego).+mobile|avantgo|bada\/|blackberry|blazer|compal|elaine|fennec|hiptop|iemobile|ip(hone|od)|iris|kindle|lge |maemo|midp|mmp|netfront|opera m(ob|in)i|palm( os)?|phone|p(ixi|re)\/|plucker|pocket|psp|series(4|6)0|symbian|treo|up\.(browser|link)|vodafone|wap|windows (ce|phone)|xda|xiino/i.test(a)||/1207|6310|6590|3gso|4thp|50[1-6]i|770s|802s|a wa|abac|ac(er|oo|s\-)|ai(ko|rn)|al(av|ca|co)|amoi|an(ex|ny|yw)|aptu|ar(ch|go)|as(te|us)|attw|au(di|\-m|r |s )|avan|be(ck|ll|nq)|bi(lb|rd)|bl(ac|az)|br(e|v)w|bumb|bw\-(n|u)|c55\/|capi|ccwa|cdm\-|cell|chtm|cldc|cmd\-|co(mp|nd)|craw|da(it|ll|ng)|dbte|dc\-s|devi|dica|dmob|do(c|p)o|ds(12|\-d)|el(49|ai)|em(l2|ul)|er(ic|k0)|esl8|ez([4-7]0|os|wa|ze)|fetc|fly(\-|_)|g1 u|g560|gene|gf\-5|g\-mo|go(\.w|od)|gr(ad|un)|haie|hcit|hd\-(m|p|t)|hei\-|hi(pt|ta)|hp( i|ip)|hs\-c|ht(c(\-| |_|a|g|p|s|t)|tp)|hu(aw|tc)|i\-(20|go|ma)|i230|iac( |\-|\/)|ibro|idea|ig01|ikom|im1k|inno|ipaq|iris|ja(t|v)a|jbro|jemu|jigs|kddi|keji|kgt( |\/)|klon|kpt |kwc\-|kyo(c|k)|le(no|xi)|lg( g|\/(k|l|u)|50|54|\-[a-w])|libw|lynx|m1\-w|m3ga|m50\/|ma(te|ui|xo)|mc(01|21|ca)|m\-cr|me(rc|ri)|mi(o8|oa|ts)|mmef|mo(01|02|bi|de|do|t(\-| |o|v)|zz)|mt(50|p1|v )|mwbp|mywa|n10[0-2]|n20[2-3]|n30(0|2)|n50(0|2|5)|n7(0(0|1)|10)|ne((c|m)\-|on|tf|wf|wg|wt)|nok(6|i)|nzph|o2im|op(ti|wv)|oran|owg1|p800|pan(a|d|t)|pdxg|pg(13|\-([1-8]|c))|phil|pire|pl(ay|uc)|pn\-2|po(ck|rt|se)|prox|psio|pt\-g|qa\-a|qc(07|12|21|32|60|\-[2-7]|i\-)|qtek|r380|r600|raks|rim9|ro(ve|zo)|s55\/|sa(ge|ma|mm|ms|ny|va)|sc(01|h\-|oo|p\-)|sdk\/|se(c(\-|0|1)|47|mc|nd|ri)|sgh\-|shar|sie(\-|m)|sk\-0|sl(45|id)|sm(al|ar|b3|it|t5)|so(ft|ny)|sp(01|h\-|v\-|v )|sy(01|mb)|t2(18|50)|t6(00|10|18)|ta(gt|lk)|tcl\-|tdg\-|tel(i|m)|tim\-|t\-mo|to(pl|sh)|ts(70|m\-|m3|m5)|tx\-9|up(\.b|g1|si)|utst|v400|v750|veri|vi(rg|te)|vk(40|5[0-3]|\-v)|vm40|voda|vulc|vx(52|53|60|61|70|80|81|83|85|98)|w3c(\-| )|webc|whit|wi(g |nc|nw)|wmlb|wonu|x700|yas\-|your|zeto|zte\-/i.test(a.substr(0,4)))check = true})(navigator.userAgent||navigator.vendor||window.opera);
+// 		return check;
+// 	}
+
+// 	function init() {
+
+// 		var container = document.getElementById( 'st-container' ),
+// 			buttons = Array.prototype.slice.call( document.querySelectorAll( '#st-trigger-effects .menu-button' ) ),
+// 			// event type (if mobile use touch events)
+// 			eventtype = mobilecheck() ? 'touchstart' : 'click',
+// 			resetMenu = function() {
+// 				classie.remove( container, 'st-menu-open' );
+// 			},
+// 			bodyClickFn = function(evt) {
+// 				if( !hasParentClass( evt.target, 'st-menu' ) ) {
+// 					resetMenu();
+// 					document.removeEventListener( eventtype, bodyClickFn );
+// 				}
+// 			};
+
+// 		buttons.forEach( function( el, i ) {
+// 			var effect = el.getAttribute( 'data-effect' );
+
+// 			el.addEventListener( eventtype, function( ev ) {
+// 				ev.stopPropagation();
+// 				ev.preventDefault();
+// 				container.className = 'st-container'; // clear
+// 				classie.add( container, effect );
+// 				setTimeout( function() {
+// 					classie.add( container, 'st-menu-open' );
+// 				}, 25 );
+// 				document.addEventListener( eventtype, bodyClickFn );
+// 			});
+// 		} );
+
+// 	}
+
+// 	init();
+
+// })();
